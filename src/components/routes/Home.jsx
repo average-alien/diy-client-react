539e123b2d8092241a5a13caf6e39fa258543a13
average-alien <allentang113@gmail.com>
2022-10-05 17:41:15 -0700

    finshes home component

--- src/components/routes/Home.jsx
+++ src/components/routes/Home.jsx
@@ -3,9 +3,37 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 export default function Home() {
+    const [blogs, setBlogs] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
+
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/blogs`)
+                setBlogs(response.data)
+            } catch (error) {
+                console.warn(error)
+                if (error.response) {
+                    setErrorMessage(error.response.data.message)
+                }
+            }
+        }
+        fetchBlogs()
+    }, [])
+
     return (
         <div>
-            Home Component
+            <h1>Welcome to my Blog</h1>
+
+            <p>{errorMessage}</p>
+
+            {blogs.map(blog => {
+                return (
+                    <h2>
+                        <Link to={`/blogs/${blog._id}`}>{blog.title}</Link>
+                    </h2>
+                )
+            })}
         </div>
     )
 }
